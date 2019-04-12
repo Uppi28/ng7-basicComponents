49e2876ad320772a2e29d9b3365ba856137fb54a
Upendra Ramaswamy <maf@DES000447s-iMac.local>
2019-04-12 19:58:00 +0530

    Directives - till ngFor

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,22 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   // selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers',
   templateUrl: 'servers.component.html',
-  // template: `
-  //   <app-server></app-server>
-  //   <app-server></app-server>`,
-  styleUrls: ['./servers.component.css']
+  styles: [`
+    .online {
+      color: white
+    }
+  `]
 })
 export class ServersComponent implements OnInit {
 
-  allowNewServer = false
+  allowNewServer = false;
   serverCreationStatus = 'No server was created';
-  serverName = "Test server"
+  serverName = 'Test server';
+  serverCreated = false;
+  servers = ['Test server', 'Test Server 2'];
 
-  constructor() { 
+  constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
@@ -26,11 +28,13 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created: Name is:' + this.serverName;
   }
 
   onUpdateServerName(event: Event) {
-    this.serverName = (<HTMLInputElement>event.target).value
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
